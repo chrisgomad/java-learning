84d4d793ccd100e5836b2f15e0978351e7050dcc
Christopher Rodriguez <chrissito.rodriguez@gmail.com>
2024-05-17 17:35:07 -0400

    added starter code for challenge exercise.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isOnRepeat = true;
-        while(isOnRepeat) {
-            System.out.println("Playing current song");
-            System.out.println("Would you like to take this off of repeat? If so, answer yes");
-            String userInput = input.next();
+        String question = "What is the make and model of your first car?";
+        String choiceOne = "A. Honda Civic ";
+        String choiceTwo = " B. Hyundai Elantra ";
+        String choiceThree = " C. Tesla Model 3 ";
 
-            if(userInput.equals("yes")) {
-                isOnRepeat = false;
-            }
-        }
-        System.out.println("Playing next song");
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        System.out.println(question);
+        // Write a print statement giving the answer choices
+        System.out.println(choiceOne + choiceTwo + choiceThree);
+        // Have the user input an answer
+        // Retrieve the user's input
+
+        // If the user's input matches the correctAnswer...
+        // then the user is correct and we want to print out a congrats message to the user.
+
+        // If the user's input does not match the correctAnswer...
+        // then the user is incorrect and we want to print out a message saying that the user is incorrect.
         }
     }
