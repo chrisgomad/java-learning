0d0d1611241ef0c50114e8a8ee1cdadeb4d73214
Christopher Rodriguez <chrissito.rodriguez@gmail.com>
2024-05-20 10:10:04 -0400

    completed challenge for creating multiple choice code.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String question = "What is the make and model of your first car?";
+        String question = "What is the make and model of your first car? (You can type just the letters for the answer ie. A, B, C)";
         String choiceOne = "A. Honda Civic ";
         String choiceTwo = " B. Hyundai Elantra ";
         String choiceThree = " C. Tesla Model 3 ";
@@ -15,12 +15,22 @@ public class Main {
         // Write a print statement giving the answer choices
         System.out.println(choiceOne + choiceTwo + choiceThree);
         // Have the user input an answer
+        Scanner input = new Scanner(System.in);
         // Retrieve the user's input
-
+        String userInput = input.next();
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
+        if(userInput.equalsIgnoreCase("B")){ // .equalsIgnoreCase can be used to ignore input's case.
+            // then the user is correct and we want to print out a congrats message to the user.
+            System.out.println("Correct! Congratulations!");
+        } else {
+            // If the user's input does not match the correctAnswer...
+            // then the user is incorrect and we want to print out a message saying that the user is incorrect.
+            System.out.println("Sorry, that is not the correct answer. Try again.");
 
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect.
+        }
+
+
+
+
         }
     }
