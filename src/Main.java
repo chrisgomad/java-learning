e6f28e08cd338ea09abfc7f72f27ad955420013b
Christopher Rodriguez <chrissito.rodriguez@gmail.com>
2024-05-23 12:35:33 -0400

    completed some exercises for practicing with functions.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void announceDeveloperTeaTime(){
-        System.out.println("Waiting for developer tea time...");
-        System.out.println("Type in a random word and press Enter to start developer tea time.");
-        Scanner input = new Scanner(System.in);
-        input.next();
-        System.out.println("It's developer tea time!");
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate){
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        return result;
+
     }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to your new job");
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
+        System.out.println(groupTotalMealPrice); // the dot after each word indicates a built-in Java function...
+        // it's good practice to search through documentation for something you want to execute, like calculating...
+        // the square root of a number, and see if there is already an existing function that can do that for you.
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
 
-        announceDeveloperTeaTime();
-
-        System.out.println("Write code");
-        System.out.println("Review code");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("Get promoted!");
     }
 
 }
